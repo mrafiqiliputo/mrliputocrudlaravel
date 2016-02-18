90b804b29229784ef7d861b0cb46f7e77c89925c
isanmasiv <isanmasiv@ymail.com>
2016-02-19 03:01:14 +0700

    Update scaffoldApp.blade.php

--- resources/views/scaffoldApp.blade.php
+++ resources/views/scaffoldApp.blade.php
@@ -76,22 +76,8 @@
                 {!! $scaffold->render() !!}
                 <div class="pushDown"></div>
                 <span>Scaffold-interface <span class = 'scaffoldv orange white-text'>v1.3.x</span></span>
-                <p class = 'light'>Copyright (c) {{date('Y')}} Amrani Houssian<br><br>
-                    Permission is hereby granted, free of charge, to any person obtaining a copy
-                    of this software and associated documentation files (the "Scaffold-Interface"), to deal
-                    in the Software without restriction, including without limitation the rights
-                    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-                    copies of the Software, and to permit persons to whom the Software is
-                    furnished to do so, subject to the following conditions:<br><br>
-                    The above copyright notice and this permission notice shall be included in
-                    all copies or substantial portions of the Software.<br><br>
-                    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-                    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-                    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-                    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-                    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-                    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-                    THE SOFTWARE.
+                <p class = 'light'> RAFIQI<br><br>
+                    Bebas Di Clone dan di modifikasi. ada kesulitan tanyakan ke www.facebook.com/mrliputo.blog
                 </p>
                 <div class = 'message'></div>
             </div>
